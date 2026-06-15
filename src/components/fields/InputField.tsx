ad0f3e2e81537ba89a35367af8e302af48a36b10
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-15 12:53:09 +0530

    create reusable input field

---
+++ src/components/fields/InputField.tsx
@@ -0,0 +1,66 @@
+import { Field, FieldDescription, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupInput,
+} from '@/components/ui/input-group';
+import * as React from 'react';
+
+interface InputFieldProps {
+	id: string;
+	label: string;
+	placeholder?: string;
+	type?: string;
+	description?: string;
+	disabled?: boolean;
+	invalid?: boolean;
+	required?: boolean;
+	endAddon?: React.ReactNode;
+}
+
+export function InputField({
+	id,
+	label,
+	type = 'text',
+	placeholder = '',
+	description = '',
+	disabled = false,
+	invalid = false,
+	required = false,
+	endAddon,
+}: InputFieldProps) {
+	return (
+		<Field
+			data-disabled={disabled ? '' : undefined}
+			data-invalid={invalid ? '' : undefined}>
+			<FieldLabel htmlFor={id}>
+				{label}
+				{required && <span className='text-destructive'>*</span>}
+			</FieldLabel>
+
+			{endAddon ?
+				<InputGroup>
+					<InputGroupInput
+						id={id}
+						type={type}
+						placeholder={placeholder}
+						disabled={disabled}
+						aria-invalid={invalid}
+					/>
+
+					<InputGroupAddon align='inline-end'>{endAddon}</InputGroupAddon>
+				</InputGroup>
+			:	<Input
+					id={id}
+					type={type}
+					placeholder={placeholder}
+					disabled={disabled}
+					aria-invalid={invalid}
+				/>
+			}
+
+			{description && <FieldDescription>{description}</FieldDescription>}
+		</Field>
+	);
+}
