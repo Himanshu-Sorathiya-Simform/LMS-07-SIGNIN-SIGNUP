aab0352f1150a65e79099d919567eebd29435744
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-15 15:17:24 +0530

    create signin schema by zod

---
+++ src/schemas/SigninSchema.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const signinSchema = z.object({
+	email: z
+		.string()
+		.trim()
+		.toLowerCase()
+		.min(1, { message: "Email address is required" })
+		.pipe(z.email({ message: "Please enter a valid email address" })),
+
+	password: z
+		.string()
+		.min(1, { message: "Password is required" })
+		.min(8, { message: "Password must be at least 8 characters" })
+		.max(18, { message: "Password must not exceed 18 characters" })
+		.regex(/[A-Z]/, {
+			message: "Password must contain at least one uppercase letter",
+		})
+		.regex(/[a-z]/, {
+			message: "Password must contain at least one lowercase letter",
+		})
+		.regex(/[0-9]/, { message: "Password must contain at least one number" })
+		.regex(/[^A-Za-z0-9]/, {
+			message: "Password must contain at least one special character",
+		})
+		.refine((val) => !/[<>]/.test(val), {
+			message: "Password contains invalid characters",
+		}),
+});
+
+type SigninSchema = z.infer<typeof signinSchema>;
+
+export { type SigninSchema, signinSchema };
